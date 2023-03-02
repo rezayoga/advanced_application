ace7fe3ce0c838d4257a6070607413752ba41e20
Reza Yogaswara <reza.yoga@gmail.com>
2023-03-02 14:24:31 +0700

    Updating

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
 
 
 def create_app() -> FastAPI:
@@ -9,6 +10,51 @@ def create_app() -> FastAPI:
 
     @app.get("/")
     async def root():
-        return {"message": "Hello Python"}
+        return {"message": "Hello World!"}
+
+    html = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Chat</title>
+        </head>
+        <body>
+            <h1>WebSocket Chat</h1>
+            <form action="" onsubmit="sendMessage(event)">
+                <input type="text" id="messageText" autocomplete="off"/>
+                <button>Send</button>
+            </form>
+            <ul id='messages'>
+            </ul>
+            <script>
+                var ws = new WebSocket("wss://app.rezayogaswara.dev/ws");
+                ws.onmessage = function(event) {
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                };
+                function sendMessage(event) {
+                    var input = document.getElementById("messageText")
+                    ws.send(input.value)
+                    input.value = ''
+                    event.preventDefault()
+                }
+            </script>
+        </body>
+    </html>
+    """
+
+    @app.get("/ws_client")
+    async def get():
+        return HTMLResponse(html)
+
+    @app.websocket("/ws")
+    async def websocket_endpoint(websocket: WebSocket):
+        await websocket.accept()
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
 
     return app
