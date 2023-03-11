e1ee606b8477741dd95fe5e929ce68a36f96becb
Reza Yogaswara <reza.yoga@gmail.com>
2023-03-11 23:40:56 +0700

    Updating

--- project/__init__.py
+++ project/__init__.py
@@ -123,7 +123,7 @@ def create_app() -> FastAPI:
         if polls:
             data = [_._asdict() for _ in polls]
             for poll in data:
-                html += f"""<button id=\"btn-vote-1\" disabled onclick=\"vote()\">{poll['option']}</button>"""
+                html += f"""<button id=\"btn-vote-{poll['p_id']}\" disabled onclick=\"vote()\">{poll['option']}</button>"""
 
 
 
