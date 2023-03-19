ef50a9a5f90f3497cffd03f6a6260d84a92a7821
Reza Yogaswara <reza.yoga@gmail.com>
2023-03-19 21:04:19 +0700

    Updating

--- project/schemas.py
+++ project/schemas.py
@@ -67,7 +67,7 @@ class Notification(BaseModel):
                 "recipients": ["b001",
                                "b002",
                                "b003"],
-                "message": {"data": [
+                "message": {[
                     {"type": "text", "text": "Hello World"},
                     {"type": "image", "url": "https://example.com/image.png"},
                     {"type": "button", "text": "Click Me", "url": "https://example.com"},
